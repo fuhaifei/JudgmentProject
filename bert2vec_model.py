fb6cf06905492e46a99a0ba5162c47089fb10565
付海飞 <793515331@qq.com>
2021-07-21 10:52:17 +0800

    完成了fine-tuning的相关代码

--- bert2vec_model.py
+++ bert2vec_model.py
@@ -1,9 +1,7 @@
 import time
 import torch
 import torch.nn as nn
-from torch.utils.data import TensorDataset
-from torch.utils.data import DataLoader
-from torch.utils.data import RandomSampler
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, random_split
 from transformers import BertModel, BertTokenizer, BertForSequenceClassification
 
 from preprocess import load_label_data
@@ -13,10 +11,10 @@ MODEL_NAME = "hfl/chinese-bert-wwm-ext"
 MAX_SEQ_LENGTH = 150
 
 # fine-tuning 的序列长度
-BATCH_SIZE = 20
+BATCH_SIZE = 10
 EPOCHS = 100
-
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LR = 1e-05
 
 
 def get_cls_vec(docs):
@@ -60,10 +58,12 @@ def prepare_train_data(sentences, tokenizer, max_seq_length=MAX_SEQ_LENGTH):
         if len(sentence_tokens) > max_seq_length - 2:
             sentence_tokens = sentence_tokens[:max_seq_length - 2]
         sentence_tokens = ['[CLS]'] + sentence_tokens + ['[SEP]']
+        sentence_tokens = tokenizer.convert_tokens_to_ids(sentence_tokens)
         # 获取对应的mask和segment()
         sentence_padding = [0] * (max_seq_length - len(sentence_tokens))
         sentence_mask = [1] * len(sentence_tokens) + sentence_padding
         sentence_type_ids = [0] * len(sentence_tokens) + sentence_padding
+        sentence_tokens += sentence_padding
         result_tokens.append(sentence_tokens)
         result_masks.append(sentence_mask)
         result_token_ids.append(sentence_type_ids)
@@ -84,11 +84,16 @@ def get_iter_dataset(sentence_tokens, sentence_labels, sentence_masks, sentence_
     sentence_labels_tensor = torch.LongTensor(sentence_labels)
     sentence_masks_tensor = torch.LongTensor(sentence_masks)
     sentence_type_ids_tensor = torch.LongTensor(sentence_type_ids)
-    train_dataset = TensorDataset(sentence_tokens_tensor, sentence_labels_tensor, sentence_masks_tensor,
+    all_dataset = TensorDataset(sentence_tokens_tensor, sentence_labels_tensor, sentence_masks_tensor,
                                   sentence_type_ids_tensor)
-    ids_sampler = RandomSampler(train_dataset)
-    train_iter = DataLoader(train_dataset, batch_size=batch_size, sampler=ids_sampler)
-    return train_iter
+    train_size = int(len(sentence_tokens) * 0.8)
+    test_size = len(sentence_tokens) - train_size
+    train_dataset, test_dataset = random_split(all_dataset, [train_size, test_size])
+    train_sampler = RandomSampler(train_dataset)
+    test_sampler = RandomSampler(test_dataset)
+    train_iter = DataLoader(train_dataset, batch_size=batch_size, sampler=train_sampler)
+    test_iter = DataLoader(test_dataset, batch_size=batch_size, sampler=test_sampler)
+    return train_iter, test_iter
 
 
 def fine_tuning_model(net, loss_func, optimizer, train_iter, test_iter, num_epochs=EPOCHS, device=DEVICE):
@@ -107,16 +112,17 @@ def fine_tuning_model(net, loss_func, optimizer, train_iter, test_iter, num_epoc
     if device is None:
         device = list(net.parameters())[0].device
     net.to(device)
-    # 开启训练模式
-    net.train()
+
     # fine_tuning_model
     for i in range(num_epochs):
+        # 开启训练模式
+        net.train()
         totalNumber = 0
         correctNumber = 0
         totalLoss = 0
         time_start = time.time()
         for sentence_tokens, sentence_labels, sentence_masks, sentence_type_ids in train_iter:
-            prediction = net(sentence_tokens.to(device), sentence_masks.to(device), sentence_type_ids.to(device))
+            prediction = net(sentence_tokens.to(device), sentence_masks.to(device), sentence_type_ids.to(device)).logits
             loss = loss_func(prediction, sentence_labels.to(device))
             loss.backward()
             optimizer.step()
@@ -141,18 +147,46 @@ def eval_net(net, test_iter, device=DEVICE):
     """
     if device is None:
         device = list(net.parameters())[0].device
+    net.to(device)
     net.eval()
     totalNumber = 0
     correctNumber = 0
     for sentence_tokens, sentence_labels, sentence_masks, sentence_type_ids in test_iter:
-        prediction = net(sentence_tokens.to(device), sentence_masks.to(device), sentence_type_ids.to(device))
+        prediction = net(sentence_tokens.to(device), sentence_masks.to(device), sentence_type_ids.to(device)).logits
         totalNumber += sentence_tokens.shape[0]
         correctNumber += (prediction.argmax(dim=1) == sentence_labels.to(device)).cpu().sum().item()
     return correctNumber / totalNumber
 
+
+# # 初始化数据
 # docs, labels = load_label_data()
 # doc_flatten = [sentence for doc in docs for sentence in doc]
 # doc_labels_flatten = [sentence for doc in labels for sentence in doc]
 # tokenizer = BertTokenizer.from_pretrained(MODEL_NAME)
+# result_tokens, result_masks, result_token_ids = prepare_train_data(doc_flatten, tokenizer, max_seq_length=MAX_SEQ_LENGTH)
+# train_iter, test_iter = get_iter_dataset(result_tokens, doc_labels_flatten, result_masks, result_token_ids, batch_size=BATCH_SIZE)
+#
+# # 初始化模型
 # net = BertForSequenceClassification.from_pretrained(MODEL_NAME, num_labels=11)
 # loss_func = nn.CrossEntropyLoss()
+# param_optimizer = list(net.named_parameters())
+# no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+# # 避免对bias 和 layerNorm层正则化
+# optimizer_grouped_parameters = [
+#     {
+#         'params':
+#         [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+#         'weight_decay':
+#         0.01
+#     },
+#     {
+#         'params':
+#         [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+#         'weight_decay':
+#         0.0
+#     }
+# ]
+# optimizer = torch.optim.Adam(optimizer_grouped_parameters, lr=LR)
+#
+# fine_tuning_model(net, loss_func, optimizer, train_iter, test_iter, num_epochs=EPOCHS)
+# eval_net(net, test_iter, device=DEVICE)
